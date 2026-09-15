9c1dc3a63ee9353a2378c8ab826088c58da93418
engoB <71884956+engoB@users.noreply.github.com>
2026-09-15 21:24:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'multiarena-v4.3.3';
+const CACHE = 'multiarena-v4.3.4';
 const SHELL = ['./', './index.html', './atelier.html', './manifest.webmanifest',
   './icons/icon-192.png', './icons/icon-512.png', './icons/icon-512-maskable.png', './icons/apple-touch-icon.png'];
 self.addEventListener('install', e => {
